12088235d4071092f42b6abc1ec7e021663ed7f8
Lan Gojkovič <langojkovic@gmail.com>
2026-02-23 22:17:22 +0100

    Fixes on DB and port forwarding

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,14 +1,22 @@
-import nodemailer from "nodemailer";
 import { env } from "../config/env.js";
 
-const transporter = nodemailer.createTransport({
-  host: env.smtp.host,
-  port: env.smtp.port,
-  secure: env.smtp.secure,
-  auth: { user: env.smtp.user, pass: env.smtp.pass },
-});
-
 export async function sendEmail({ to, subject, html }) {
+  if (env.nodeEnv !== "production") {
+    console.log("DEV_EMAIL_TO:", to);
+    console.log("DEV_EMAIL_SUBJECT:", subject);
+    console.log("DEV_EMAIL_HTML:", html);
+    return;
+  }
+
+  const nodemailer = await import("nodemailer");
+
+  const transporter = nodemailer.default.createTransport({
+    host: env.smtp.host,
+    port: env.smtp.port,
+    secure: env.smtp.secure,
+    auth: { user: env.smtp.user, pass: env.smtp.pass },
+  });
+
   await transporter.sendMail({
     from: env.smtp.from,
     to,
